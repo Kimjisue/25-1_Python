19a404630984b0e4c21ceb0a9458968a0d4db1f0
Kimjisue <22100194@handong.ac.kr>
2025-03-31 15:11:21 +0900

    commit

--- week5.py
+++ week5.py
@@ -2,18 +2,18 @@
 ####월요일
 
 #숫자 입력 
-score = int(input("숫자를 입력하세요 : "))
+# score = int(input("숫자를 입력하세요 : "))
 
-if score >= 90 and score <= 100:
-    print("A")
-elif score <= 89 and score >= 80:
-    print("B")
-elif score <= 79 and score >= 70:
-    print("C")
-elif score <= 69 and score >= 60:
-    print("D")
-elif score <=59:
-    print("F")
+# if score >= 90 and score <= 100:
+#     print("A")
+# elif score <= 89 and score >= 80:
+#     print("B")
+# elif score <= 79 and score >= 70:
+#     print("C")
+# elif score <= 69 and score >= 60:
+#     print("D")
+# elif score <=59:
+#     print("F")
 
 ###################################################
 #양의 정수를 입력 받아 아래와 같이 출력 
@@ -22,12 +22,22 @@ elif score <=59:
 #5의 배수이면 5의 배수 
 #2의 배수이면서 5의 배수이면 2,5의 공배수 
 
-num = int(input("양의 정수를 입력하세요 : "))
-if num <= 0:
-    print("입력오류")
-elif num % 2 == 0 and num % 5 == 0:
-    print("2와 5의 공배수")
-elif num % 2 == 0:
-    print("2의 배수")
-elif num % 5 == 0:
-    print("5의 배수")
+# num = int(input("양의 정수를 입력하세요 : "))
+# if num <= 0:
+#     print("입력오류")
+# elif num % 2 == 0 and num % 5 == 0:
+#     print("2와 5의 공배수")
+# elif num % 2 == 0:
+#     print("2의 배수")
+# elif num % 5 == 0:
+#     print("5의 배수")
+
+#list 
+지갑 = ["돈","사진","카드","신분증","쿠폰","아이돌사진"]
+
+#둘은 같음 
+print(지갑[0])
+print(지갑[-0])
+
+#사진, 카드 출력 
+print(지갑[1:3])
