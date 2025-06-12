c826ba9d84159fee71cc11e232df3ed7089033d2
Kimjisue <22100194@handong.ac.kr>
2025-06-12 17:12:07 +0900

    commit

--- week15.py
+++ week15.py
@@ -40,35 +40,47 @@
 # window.mainloop()
 
 
-import tkinter as tk
+# import tkinter as tk
 
-def button_click():
-    name1 = entry1.get()
-    name2 = entry2.get()
-    label3.config(text=f"{name1}랑 {name2}랑 사귄다 !")
+# def button_click():
+#     name1 = entry1.get()
+#     name2 = entry2.get()
+#     label3.config(text=f"{name1}랑 {name2}랑 사귄다 !")
 
-window = tk.Tk()
-window.title("닥또리")
-window.geometry("400x200+500+150")
+# window = tk.Tk()
+# window.title("닥또리")
+# window.geometry("400x200+500+150")
 
-# 첫 번째 이름 입력
-label1 = tk.Label(window, text="이름1")
-label1.grid(row=0, column=0, padx=10, pady=5)
-entry1 = tk.Entry(window)
-entry1.grid(row=1, column=0, padx=10, pady=5)
+# # 첫 번째 이름 입력
+# label1 = tk.Label(window, text="이름1")
+# label1.grid(row=0, column=0, padx=10, pady=5)
+# entry1 = tk.Entry(window)
+# entry1.grid(row=1, column=0, padx=10, pady=5)
 
-# 두 번째 이름 입력 (테스트)
-label2 = tk.Label(window, text="이름2")
-label2.grid(row=0, column=1, padx=10, pady=5)
-entry2 = tk.Entry(window)
-entry2.grid(row=1, column=1, padx=10, pady=5)
+# # 두 번째 이름 입력 (테스트)
+# label2 = tk.Label(window, text="이름2")
+# label2.grid(row=0, column=1, padx=10, pady=5)
+# entry2 = tk.Entry(window)
+# entry2.grid(row=1, column=1, padx=10, pady=5)
 
-# 버튼
-button = tk.Button(window, text="버튼", width=20, command=button_click)
-button.grid(row=2, column=0, columnspan=2, pady=10)
+# # 버튼
+# button = tk.Button(window, text="버튼", width=20, command=button_click)
+# button.grid(row=2, column=0, columnspan=2, pady=10)
 
-# 결과 라벨
-label3 = tk.Label(window, text="")
-label3.grid(row=3, column=0, columnspan=2, pady=5)
+# # 결과 라벨
+# label3 = tk.Label(window, text="")
+# label3.grid(row=3, column=0, columnspan=2, pady=5)
 
-window.mainloop()
+# window.mainloop()
+
+
+# # ##############################################
+# # ##############################################
+# # ##############################################
+# # ######### 목 
+
+
+a = [["a","b","멜론"],"오이","가지"]
+len(a[0]) #(a,b,멜론)
+
+print(len(a)) #a를 출력 
